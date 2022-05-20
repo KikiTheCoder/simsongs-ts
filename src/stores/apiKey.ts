aa1424d976fa8535b4adf3d5f05c43418cb6aa73
KikiTheCoder <kikilp07@web.de>
2022-05-20 18:39:44 +0200

    bruh

--- src/stores/apiKey.ts
+++ src/stores/apiKey.ts
@@ -3,6 +3,6 @@ import { defineStore } from 'pinia'
 export const useApiKeyStore = defineStore({
   id: 'apiKey',
   state: (): {[key: string]: string} => ({
-    key: import.meta.env.VITE_API_KEY
+    key: "6919f5aa4f73b1980cf46715fd153031"
   })
 })
